52e5d8a7951a8a8ae53b19924b629d8ca756222e
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-03-06 19:51:43 -0500

    setUp register back/frontend logic

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import {Link} from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
 
 const Header = () =>{
+  const {isLoggedIn} = useAppContext();
   return (
     <div className="bg-green-600 py-6">
         <div className="container mx-auto flex justify-between">
@@ -9,7 +11,14 @@ const Header = () =>{
             </span>
 
             <span className="flex space-x-2">
-               <Link to="/sign-in" className="text-xs flex items-center text-white px-3 font-bold hover:text-gray-500 ">Sign In</Link>
+              {isLoggedIn ?
+               <>
+                <Link to="/my-lectures">My Lectures</Link>
+                <Link to="/my-lectures">My Books</Link>
+                <button>Sign out</button>
+              </>
+              :<Link to="/sign-in" className="text-xs flex items-center text-white px-3 font-bold hover:text-gray-500 ">Sign In</Link>}
+               
             </span>
         </div>
   
