2fc4c0e34cafc2aea3d5d3e21a3b57005dc1a3b0
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-03-07 21:42:32 -0500

    setup cloudi

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,14 @@ import authRoutes from './routes/auth';
 import cookieParser from "cookie-parser";
 import path from 'path';
 
+import {v2 as cloudinary} from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secrect:process.env.CLOUDINARY_API_SECRET,
+})
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 
