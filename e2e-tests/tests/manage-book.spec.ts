e1b6d426b49ead539a06238863210f69ce183550
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-04-02 16:02:23 -0400

    resolved test cases confilts

--- e2e-tests/tests/manage-book.spec.ts
+++ e2e-tests/tests/manage-book.spec.ts
@@ -25,7 +25,6 @@ test("should allow user to add a book", async ({page}) =>{
   await page.selectOption(`select[name="starRating"]`,"5"); 
   await page.getByText("Art").click(); 
   await page.getByLabel("16-17 years").check(); 
-  await page.getByLabel("6-7 years").check(); 
 
   await page.setInputFiles(`[name="imageFiles"]`,[
     path.join(__dirname, "files","testImage1.jpg"),
@@ -38,13 +37,13 @@ test("should allow user to add a book", async ({page}) =>{
 
 test("should display books", async({page}) =>{
   await page.goto(`${UI_URL}mybooks`);
-  await expect(page.getByText("Designing Data-Intensive Applications")).toBeVisible();
-  await expect(page.getByText("Data is at the center of many challenges in system design today.")).toBeVisible();
-  await expect(page.getByText("Montréal,NDG")).toBeVisible();
-  await expect(page.getByText("Reference")).toBeVisible();
-  await expect(page.getByText("CAD2 per week")).toBeVisible();
-  await expect(page.getByText("16-17 years18 years and up")).toBeVisible();
-  await expect(page.getByText("4 star rating")).toBeVisible();
+  await expect(page.getByText("Test Add Book").first()).toBeVisible();
+  await expect(page.getByText("Test description for add book!").first()).toBeVisible();
+  await expect(page.getByText("Test City,Test Community").first()).toBeVisible();
+  await expect(page.getByText("Art").first()).toBeVisible();
+  await expect(page.getByText("CAD20 per week").first()).toBeVisible();
+  await expect(page.getByText("16-17 years").first()).toBeVisible();
+  await expect(page.getByText("5 star rating").first()).toBeVisible();
 
   await expect(page.getByRole("link", {name: "View Details"}).first()).toBeVisible();
   await expect(page.getByRole("link", {name: "Add Book"})).toBeVisible();
@@ -58,7 +57,7 @@ test("should edit book", async({page}) =>{
   await page.getByRole("link", {name: "View Details"}).first().click();
   await page.waitForSelector('[name="name"]', {state: "attached"});
 
-  await expect(page.locator('[name="name"]')).toHaveValue('Test Add Book');
+  await expect(page.locator('[name="name"]')).toHaveValue("Test Add Book");
   await page.locator('[name="name"]').fill("Test Add Book UPDATED")
   await page.getByRole("button", {name: "Save"}).click();
 
@@ -67,5 +66,6 @@ test("should edit book", async({page}) =>{
   //check if name still remains updated after page reload.
   await page.reload();
   await expect(page.locator('[name="name"]')).toHaveValue("Test Add Book UPDATED");
-  await page.locator('[name="name"]').fill('Test Add Book');
+  await page.locator('[name="name"]').fill("Test Add Book");
+  await page.getByRole("button", {name:"Save"}).click();
 })
