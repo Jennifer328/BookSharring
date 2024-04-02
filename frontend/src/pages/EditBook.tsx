ffdba65d27cf68a5189d24984cfc4ef88ec4841a
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-04-02 14:56:00 -0400

    add Toast on EditBook

--- frontend/src/pages/EditBook.tsx
+++ frontend/src/pages/EditBook.tsx
@@ -2,12 +2,14 @@ import { useMutation, useQuery } from "react-query";
 import { useParams } from "react-router-dom"
 import * as apiClient from "../api-client";
 import ManageBookForm from "../forms/ManageBookForm/ManageBookForm";
+import { useAppContext } from "../contexts/AppContext";
 
 
 
 const EditBook = () => {
 
   const {bookId} = useParams();
+  const {showToast} = useAppContext();
   const {data: book} =useQuery("fetchMyBookById",
    ()=> apiClient.fetchMyBookById(bookId || ""),{
     enabled: !!bookId, //check if the bookId is a valid value, if yes enabled will be set to true
@@ -15,10 +17,10 @@ const EditBook = () => {
 
   const {mutate,isLoading} = useMutation(apiClient.updateMyBookById,{
     onSuccess: ()=>{
-       
+       showToast({message: "Book Saved!", type: "SUCCESS"});
     },
     onError: ()=>{
-
+       showToast({message: "Error saving Book", type: "ERROR"});
     }
   });
 
