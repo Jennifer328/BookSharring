857dd84d7f69b299fe1f19fc40c35dbae11944d1
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-04-28 22:58:23 -0400

    add sorting logic in backend

--- backend/src/routes/books.ts
+++ backend/src/routes/books.ts
@@ -8,9 +8,25 @@ const router = express.Router();
 
 // /api/books/search?
 router.get("/search", async (req: Request, res:Response) =>{
+
     try{
-     
       const query = constructSearchQuery(req.query);
+
+      let sortOptions = {};
+
+      switch(req.query.sortOption){
+        case "starRating":
+          sortOptions = {starRating: -1};
+          break;
+
+        case "pricePerWeekAsc":
+          sortOptions = {pricePerWeek: 1};
+          break;
+
+        case "pricePerWeekDesc":
+          sortOptions = {pricePerWeek : -1};
+          break;
+      }
 
       const pageSize = 5;
       const pageNumber = parseInt(req.query.page ? req.query.page.toString() : "1" ); //check if there is a pageNumber params if not set it to 1
@@ -18,7 +34,7 @@ router.get("/search", async (req: Request, res:Response) =>{
       //number of items needed to be skipped
       const skip = (pageNumber - 1) * pageSize;
 
-      const books = await Book.find().skip(skip).limit(pageSize);
+      const books = await Book.find(query).sort(sortOptions).skip(skip).limit(pageSize);
 
       const total = await Book.countDocuments(query);
       const response : BookSearchResponse = {
@@ -46,6 +62,28 @@ const constructSearchQuery = (queryParams: any) => {
       { city: new RegExp(queryParams.city, "i") },
    
     ];
+  }
+
+  if(queryParams.types){ //the user can select more than one type once
+    constructedQuery.type = {
+      $in: Array.isArray(queryParams.types)
+        ? queryParams.types
+        : [queryParams.types],//check if the user input one or more types
+    };
+  }
+
+  if(queryParams.stars){
+    const starRatings = Array.isArray(queryParams.stars)
+      ? queryParams.stars.map((star:string) => parseInt(star))
+      : parseInt(queryParams.stars);
+
+      constructedQuery.starRating = {$in: starRatings};
+  }
+
+  if(queryParams.maxPrice){
+    constructedQuery.pricePerWeek ={
+      $lte: parseInt(queryParams.maxPrice).toString(),
+    }
   }
 
   
