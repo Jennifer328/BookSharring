bbb94a831bd417536f817f5e8976be2e839993f8
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-05-02 17:09:52 -0400

    add search Books test

---
+++ e2e-tests/tests/search-books.spec.ts
@@ -0,0 +1,27 @@
+import {test, expect} from "@playwright/test";
+
+
+const UI_URL = "http://localhost:5173/";
+
+test.beforeEach(async({page}) =>{
+  await page.goto(UI_URL);
+  //GET THE SIGNIN BUTTON
+  await page.getByRole("link",{name: "Sign In"}).click();
+  await expect(page.getByRole("heading", {name: "Sign In"})).toBeVisible();
+  await page.locator("[name=email]").fill("maimai@gmail.com");
+  await page.locator("[name=password]").fill("qazxsw");
+  await page.getByRole("button",{name: "Login"}).click();
+
+  await expect(page.getByText("Sign in Successful!")).toBeVisible();
+});
+
+
+test("Should show book search results", async({page}) =>{
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("type a city to search").fill("Laval");
+  await page.getByRole("button", {name: "Search"}).click();
+
+  await expect(page.getByText("Books found in Laval")).toBeVisible();
+  await expect(page.getByText("Pragmatic Programmer, The: Your journey to mastery, 20th Anniversary Edition")).toBeVisible();
+});
