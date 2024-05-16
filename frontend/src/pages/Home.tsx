f410d1376e1425c06fd4e1bdb18c6f772eda8478
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-05-16 14:45:47 -0400

    add HomePage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,9 +2,15 @@ import { useQuery } from "react-query";
 import * as apiClient from "../api-client";
 
 const Home = () => {
-  const {data: books} = useQuery("fetchAllBooks", apiClient.fetchAllBooks);
+  const {data: books} = useQuery("fetchAllBooks", ()=> apiClient.fetchAllBooks());
+  const topRowBooks = books?.slice(0,2) || [];
+  const bottomRowBooks = books?.slice(2) || [];
+
   return (
-    <div>THIS IS NEW Home</div>
+    <div className="space-y-3">
+         <h2 className="text-3xl font-bold">Latest Shared Books</h2>
+         <p>Most recent books added by our communities</p>
+    </div>
   )
 }
 
