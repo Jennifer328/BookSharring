7f0f7e99ffe405480317327bee43356ae37391f3
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2024-05-16 14:56:52 -0400

    add LatesBookCard

---
+++ frontend/src/components/LatestBookCard .tsx
@@ -0,0 +1,25 @@
+import { BookType } from "../../../backend/src/shared/types";
+import {Link} from "react-router-dom";
+
+type Props = {
+  book : BookType
+}
+
+const LatestBookCard  = ({book}:Props) => {
+  return (
+    <Link 
+      to={`/detail/${book._id}}`}
+      className="relative cursor-pointer overflow-hidden rounded-md">
+
+         <div className="h-[300px]">
+            <img src={book.imageUrls[0]} className="w-full h-full object-cover object-center"/>
+         </div>
+
+         <div className="absolute bottom-0 p-4 bg-black bg-opacity-50 w-full rounded-b-md">
+               <span className="text-white font-bold -tracking-tight text-3xl">{book.name}</span>
+         </div>
+    </Link>
+  )
+}
+
+export default LatestBookCard 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import * as apiClient from "../api-client";
+import LatestBookCard from "../components/LatestBookCard ";
 
 const Home = () => {
   const {data: books} = useQuery("fetchAllBooks", ()=> apiClient.fetchAllBooks());
@@ -10,6 +11,23 @@ const Home = () => {
     <div className="space-y-3">
          <h2 className="text-3xl font-bold">Latest Shared Books</h2>
          <p>Most recent books added by our communities</p>
+
+
+         <div className="grid gap-4">
+
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
+                  {topRowBooks.map((book)=>(
+                    <LatestBookCard  book={book}/>
+                  ))}
+          </div>
+
+          <div className="grid md:grid-cols-3 gap-4">
+               {bottomRowBooks.map((book)=>(
+                <LatestBookCard  book={book}/>
+               ))}
+          </div>
+
+         </div>
     </div>
   )
 }
